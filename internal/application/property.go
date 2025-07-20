d6cdb33fc6656c596be42bc106a320adf261ba59
Mohan Dutt Parashar <kraken@Mohans-MacBook-Air.local>
2025-07-20 22:57:27 +0530

    updates

--- internal/application/property.go
+++ internal/application/property.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/VI-IM/im_backend_go/ent"
 	"github.com/VI-IM/im_backend_go/ent/schema"
 	"github.com/VI-IM/im_backend_go/internal/domain"
 	"github.com/VI-IM/im_backend_go/request"
@@ -150,6 +151,79 @@ func isMetaInfoEmpty(metaInfo schema.PropertyMetaInfo) bool {
 		metaInfo.Canonical == ""
 }
 
+// Helper function to prefill property data from project information
+func prefillPropertyFromProject(project *ent.Project, property *domain.Property) {
+	if project == nil {
+		return
+	}
+
+	// Initialize WebCards if empty - we don't need to check since we're going to set fields directly
+
+	// Property Address: Inherit from project location info
+	if project.LocationInfo.GoogleMapLink != "" || project.LocationInfo.ShortAddress != "" {
+		property.WebCards.LocationMap.GoogleMapLink = project.LocationInfo.GoogleMapLink
+		property.WebCards.LocationMap.Description = project.LocationInfo.ShortAddress
+	}
+
+	// RERA Details: Copy from project's RERA info
+	if len(project.WebCards.ReraInfo.ReraList) > 0 && project.WebCards.ReraInfo.ReraList[0].ReraNumber != "" {
+		// Use first RERA entry for property RERA number
+		property.PropertyReraInfo.ReraNumber = project.WebCards.ReraInfo.ReraList[0].ReraNumber
+		property.WebCards.PropertyDetails.ReraNumber.Value = project.WebCards.ReraInfo.ReraList[0].ReraNumber
+	}
+
+	// Built Up Area: Copy from project details
+	if project.WebCards.Details.Area.Value != "" {
+		property.WebCards.PropertyDetails.BuiltUpArea.Value = project.WebCards.Details.Area.Value
+	}
+	if project.WebCards.Details.Sizes.Value != "" {
+		property.WebCards.PropertyDetails.Sizes.Value = project.WebCards.Details.Sizes.Value
+	}
+
+	// Possession Status: Copy from project possession date
+	if project.WebCards.Details.PossessionDate.Value != "" {
+		property.WebCards.PropertyDetails.PossessionStatus.Value = project.WebCards.Details.PossessionDate.Value
+	}
+
+	// Property Type: Map project type to property type
+	if project.ProjectType.String() != "" {
+		property.WebCards.PropertyDetails.PropertyType.Value = project.ProjectType.String()
+	}
+	if project.WebCards.Details.Type.Value != "" {
+		property.WebCards.PropertyDetails.PropertyType.Value = project.WebCards.Details.Type.Value
+	}
+
+	// Configuration: Copy from project
+	if project.WebCards.Details.Configuration.Value != "" {
+		property.WebCards.PropertyDetails.Configuration.Value = project.WebCards.Details.Configuration.Value
+	}
+
+	// Why To Choose (USP): Copy from project
+	if len(project.WebCards.WhyToChoose.USP_List) > 0 {
+		property.WebCards.WhyToChoose.UspList = make([]string, len(project.WebCards.WhyToChoose.USP_List))
+		copy(property.WebCards.WhyToChoose.UspList, project.WebCards.WhyToChoose.USP_List)
+	}
+	if len(project.WebCards.WhyToChoose.ImageUrls) > 0 {
+		property.WebCards.WhyToChoose.ImageUrls = make([]string, len(project.WebCards.WhyToChoose.ImageUrls))
+		copy(property.WebCards.WhyToChoose.ImageUrls, project.WebCards.WhyToChoose.ImageUrls)
+	}
+
+	// Know About: Copy from project
+	if project.WebCards.KnowAbout.Description != "" {
+		property.WebCards.KnowAbout.Description = project.WebCards.KnowAbout.Description
+	}
+
+	// Video Presentation: Copy from project
+	if len(project.WebCards.VideoPresentation.URLs) > 0 {
+		property.WebCards.VideoPresentation.Urls = make([]string, len(project.WebCards.VideoPresentation.URLs))
+		copy(property.WebCards.VideoPresentation.Urls, project.WebCards.VideoPresentation.URLs)
+	}
+
+	// Note: Amenities and Builder About are not directly copied to property schema
+	// as they are typically accessed via the project relationship
+	// However, they can be accessed through the project relationship when needed
+}
+
 func (c *application) GetPropertiesOfProject(projectID string) ([]*response.Property, *imhttp.CustomError) {
 	properties, err := c.repo.GetPropertiesOfProject(projectID)
 	if err != nil {
@@ -168,10 +242,27 @@ func (c *application) GetPropertiesOfProject(projectID string) ([]*response.Prop
 func (c *application) AddProperty(input request.AddPropertyRequest) (*response.AddPropertyResponse, *imhttp.CustomError) {
 	var property domain.Property
 
+	// Set basic property fields from request
 	property.ProjectID = input.ProjectID
 	property.Name = input.Name
 	property.PropertyType = input.PropertyType
 	property.CreatedByUserID = input.CreatedByUserID
+
+	// Fetch project data to prefill property information
+	if input.ProjectID != "" {
+		project, err := c.repo.GetProjectByID(input.ProjectID)
+		if err != nil {
+			logger.Get().Error().Err(err).Str("projectID", input.ProjectID).Msg("Failed to get project for prefilling")
+			return nil, imhttp.NewCustomErr(http.StatusInternalServerError, "Failed to get project data", err.Error())
+		}
+
+		if project == nil {
+			return nil, imhttp.NewCustomErr(http.StatusNotFound, "Project not found", "Project not found")
+		}
+
+		// Prefill property data from project information
+		prefillPropertyFromProject(project, &property)
+	}
 
 	result, err := c.repo.AddProperty(property)
 	if err != nil {
